7ddd4a2f9236061293d8669c21a4299f58d76897
Markpo2 <127012533+Markpo2@users.noreply.github.com>
2023-03-14 00:41:06 -0500

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,6 +25,6 @@ import os
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 0
+    PROCESS_MAX_TIMEOUT = 1
     # watermark file
     DEF_WATER_MARK_FILE = ""
